646075976c4e1c9e0e79f9f6930921b3f6a19e56
govinda-mahanti <gmahanti955@gmail.com>
2024-10-14 00:22:26 +0530

    add

--- src/Components/Models.js
+++ src/Components/Models.js
@@ -1,32 +1,53 @@
-import React,{useState} from 'react'
-import { Box, Button, Link, Modal, Typography, Grid } from "@mui/material"
+import React, { useState } from "react";
+import { Box, Button, Link, Modal, Typography, Grid } from "@mui/material";
 const Models = () => {
-  const [open, setOpen]=useState(false)
+  const [open, setOpen] = useState(false);
   return (
-    <div style={{marginTop:"70px"}}>
-      <Button onClick={()=>setOpen(true)}>Open Model</Button>
-      <Modal open={open} onClose={()=>setOpen(false)}>
+    <div style={{ marginTop: "70px" }}>
+      <Button onClick={() => setOpen(true)}>Open Model</Button>
+      <Modal open={open} onClose={() => setOpen(false)}>
         <Box position="absolute" top="50%" left="50%">
-
-        <Typography>This is Model</Typography>
-        <Button variant='contained' onClick={()=>setOpen(false)}>cancel</Button>
-
+          <Typography>This is Model</Typography>
+          <Button variant="contained" onClick={() => setOpen(false)}>
+            cancel
+          </Button>
         </Box>
       </Modal>
-      <Link variant='h4' color='secondary' href="https://www.youtube.com/watch?v=0euw01oikEw&t=5464s" underline='always'>
-      Nav Link
+      <Link
+        variant="h4"
+        color="secondary"
+        href="https://www.youtube.com/watch?v=0euw01oikEw&t=5464s"
+        underline="always"
+      >
+        Nav Link
       </Link>
 
-      <Grid container >
-        <Grid item xs={4} sx={{background:"red"}}>
-            Content A
+      <Grid container>
+        <Grid
+          item
+          xs={12}
+          sm={8}
+          md={6}
+          lg={3}
+          xl={1}
+          sx={{ background: "red" }}
+        >
+          Content A
         </Grid>
-        <Grid item xs={8} sx={{background:"yellow"}}>
-            Content B
+        <Grid
+          item
+          xs={12}
+          sm={8}
+          md={6}
+          lg={3}
+          xl={1}
+          sx={{ background: "yellow" }}
+        >
+          Content B
         </Grid>
       </Grid>
     </div>
-  )
-}
+  );
+};
 
-export default Models
+export default Models;
